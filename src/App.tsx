4fc5adbcf4644f0fc6aa5f9c0d17214b6a4d210d
NilsHellwig <nc.hellwig@gmail.com>
2022-08-10 19:07:11 +0200

    init react project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
