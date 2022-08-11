7b69148b22dd03dcafc16243d77e90c260c040e8
markusbink <bink-markus@web.de>
2022-08-11 16:40:46 +0200

    Remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from "react";
-import "./App.css";
-
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
-}
-
-export default App;
+};
