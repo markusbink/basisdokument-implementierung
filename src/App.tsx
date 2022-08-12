8d994bfa8873f738ca286ec67664d23a197d3b69
markusbink <bink-markus@web.de>
2022-08-12 09:02:14 +0200

    Prevent refreshing of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Auth } from "./pages/Auth";
 import { Main } from "./pages/Main";
 
+const registerKeyListener = (e: KeyboardEvent) => {
+  if (e.key === "r" && e.metaKey) {
+    e.preventDefault();
+  }
+};
+
 export const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<Boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("keydown", registerKeyListener);
+    return () => {
+      window.removeEventListener("keydown", registerKeyListener);
+    };
+  }, []);
+
   return (
     <div className="App h-screen">
       {isAuthenticated ? (
