41747479527740b60cd553f5f614cd6cf6be1a0d
NilsHellwig <nc.hellwig@gmail.com>
2022-08-12 08:59:13 +0200

    add collapsible header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 export const Header = () => {
+  const [showFoldOutMenu, setShowFoldOutMenu] = useState<Boolean>(false);
+
   return (
-    <header className="p-4">
-      <h1>Header mit Aktionen und Filter</h1>
+    <header>
+      {/* main part of the header */}
+      <div className="flex p-4">
+        <h1>Header mit Aktionen und Filter</h1>
+        <button
+          className="bg-lightGrey"
+          onClick={() => {
+            setShowFoldOutMenu(!showFoldOutMenu);
+          }}
+        >
+          Ausklappen
+        </button>
+      </div>
+      {/* fold-out part of the header */}
+      {showFoldOutMenu ? (
+        <div className="p-4 bg-offWhite">
+          <h1>Ausgeklappter Header mit Aktionen und Filter</h1>
+        </div>
+      ) : null}
     </header>
   );
 };
