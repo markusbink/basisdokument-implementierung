750a2016d14ba71d65456c81ce9aca924442ed7f
markusbink <bink-markus@web.de>
2022-08-12 20:22:38 +0200

    #1 Add entry header to new entry component

--- src/components/entry/Entry.tsx
+++ src/components/entry/Entry.tsx
@@ -1,8 +1,14 @@
 import cx from "classnames";
-import { ArrowBendLeftUp } from "phosphor-react";
-import React, { useState } from "react";
+import {
+  ArrowBendLeftUp,
+  BookmarkSimple,
+  DotsThree,
+  Notepad,
+} from "phosphor-react";
+import React, { useEffect, useState } from "react";
 import { IEntry, UserRole } from "../../types";
 import { Button } from "../Button";
+import { Action } from "./Action";
 import { EntryBody } from "./EntryBody";
 import { EntryHeader } from "./EntryHeader";
 import { NewEntry } from "./NewEntry";
@@ -19,6 +25,7 @@ export const Entry: React.FC<EntryProps> = ({
   viewedBy,
 }) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(true);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [isNewEntryVisible, setIsNewEntryVisible] = useState<boolean>(false);
 
   const isJudge = viewedBy === UserRole.Judge;
@@ -33,12 +40,52 @@ export const Entry: React.FC<EntryProps> = ({
 
   const canAddEntry = isJudge || !isOwnEntry;
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", closeMenu);
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        closeMenu();
+      }
+    });
+
+    return () => {
+      document.removeEventListener("click", closeMenu);
+    };
+  }, [isMenuOpen]);
+
   const toggleBody = (e: React.MouseEvent) => {
     setIsExpanded(!isExpanded);
   };
 
   const showNewEntry = () => {
     setIsNewEntryVisible(true);
+  };
+
+  const bookmarkEntry = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    console.log(`bookmark entry ${entry.id}`);
+  };
+
+  const addNote = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    console.log(`add note to entry ${entry.id}`);
+  };
+
+  const toggleMenu = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const editEntry = (e: React.MouseEvent) => {
+    console.log(`edit entry ${entry.id}`);
+  };
+
+  const deleteEntry = (e: React.MouseEvent) => {
+    console.log(`delete entry ${entry.id}`);
   };
 
   return (
@@ -50,11 +97,59 @@ export const Entry: React.FC<EntryProps> = ({
       >
         <div className={cx("w-1/2")}>
           <EntryHeader
-            entry={entry}
+            isPlaintiff={isPlaintiff}
             isExpanded={isExpanded}
             toggleBody={toggleBody}
-            isBookmarked={isBookmarked}
-          />
+          >
+            {/* Meta-Data */}
+            <div className="flex gap-2">
+              <span
+                className={cx("rounded-full px-3 py-1 text-xs font-semibold", {
+                  "bg-darkPurple text-lightPurple": isPlaintiff,
+                  "bg-darkPetrol text-lightPetrol": !isPlaintiff,
+                })}
+              >
+                K-1-1
+              </span>
+              <span className="font-bold">{entry.author}</span>
+              <span>25.05.2022</span>
+            </div>
+            {/* Actions */}
+            <div className="flex gap-2">
+              <Action onClick={bookmarkEntry} isPlaintiff={isPlaintiff}>
+                <BookmarkSimple
+                  size={20}
+                  weight={isBookmarked ? "fill" : "regular"}
+                />
+              </Action>
+              <Action onClick={addNote} isPlaintiff={isPlaintiff}>
+                <Notepad size={20} />
+              </Action>
+              <Action
+                className="relative"
+                onClick={toggleMenu}
+                isPlaintiff={isPlaintiff}
+              >
+                <DotsThree size={20} />
+                {isMenuOpen ? (
+                  <ul className="absolute p-2 bg-white text-darkGrey rounded-xl w-[200px] shadow-lg">
+                    <li
+                      onClick={editEntry}
+                      className="p-2 hover:bg-offWhite rounded-lg"
+                    >
+                      Bearbeiten
+                    </li>
+                    <li
+                      onClick={deleteEntry}
+                      className="p-2 hover:bg-offWhite rounded-lg"
+                    >
+                      Löschen
+                    </li>
+                  </ul>
+                ) : null}
+              </Action>
+            </div>
+          </EntryHeader>
           {isExpanded && (
             <EntryBody isPlaintiff={isPlaintiff}>{entry.text}</EntryBody>
           )}
--- src/components/entry/NewEntry.tsx
+++ src/components/entry/NewEntry.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { UserRole } from "../../types";
 import { Button } from "../Button";
 import { Action } from "./Action";
+import { EntryHeader } from "./EntryHeader";
 
 interface NewEntryProps {
   parentRole: "Kläger" | "Beklagter";
@@ -21,50 +22,70 @@ export const NewEntry: React.FC<NewEntryProps> = ({
 
   return (
     <div
-      className={cx("flex flex-col mt-4 bg-white transition-all", {
-        "w-1/2": !isExpanded,
-        "w-full": isExpanded,
-      })}
+      className={cx(
+        "flex flex-col mt-4  transition-all rounded-lg overflow-hidden bg-white",
+        {
+          "w-1/2": !isExpanded,
+          "w-full": isExpanded,
+        }
+      )}
     >
+      {/* NewEntry Header */}
+      <EntryHeader
+        className="rounded-b-none cursor-default"
+        isPlaintiff={!isPlaintiff}
+      >
+        <span className="font-bold">Stefan Schneider</span>
+      </EntryHeader>
       {/* Toolbar */}
-      <div className="p-4 flex">
-        <Action
-          onClick={() => setIsExpanded(!isExpanded)}
-          isPlaintiff={isPlaintiff}
-        >
-          {isExpanded ? <CornersIn /> : <CornersOut />}
-        </Action>
-      </div>
-      <textarea
-        className={cx("p-6 bg-white rounded-lg border min-h-[140px]", {
+      <div
+        className={cx("border overflow-hidden rounded-b-lg", {
           "border-lightPurple": !isPlaintiff,
           "border-lightPetrol": isPlaintiff,
         })}
-        placeholder="Text eingeben"
-      />
-      <div className="flex justify-between">
-        <Button
-          onClick={() => setIsNewEntryVisible(false)}
-          size="sm"
-          bgColor="transparent"
-          textColor={cx("font-bold", {
-            "text-darkPurple": isPlaintiff,
-            "text-darkPetrol": !isPlaintiff,
-          })}
-        >
-          Abbrechen
-        </Button>
-        <Button
-          onClick={() => setIsNewEntryVisible(false)}
-          size="sm"
-          bgColor="transparent"
-          textColor={cx("font-bold", {
-            "text-darkPurple": isPlaintiff,
-            "text-darkPetrol": !isPlaintiff,
-          })}
-        >
-          Speichern
-        </Button>
+      >
+        <div className=" p-4 flex">
+          <Action
+            onClick={() => setIsExpanded(!isExpanded)}
+            isPlaintiff={!isPlaintiff}
+          >
+            {isExpanded ? <CornersIn /> : <CornersOut />}
+          </Action>
+        </div>
+        <textarea
+          className={cx(
+            "p-6 min-h-[140px] w-full border-y focus:outline-none",
+            {
+              "border-lightPurple": !isPlaintiff,
+              "border-lightPetrol": isPlaintiff,
+            }
+          )}
+          placeholder="Text eingeben"
+        />
+        <div className="flex justify-between">
+          <Button
+            onClick={() => setIsNewEntryVisible(false)}
+            size="sm"
+            bgColor="transparent"
+            textColor={cx("font-bold", {
+              "text-darkPurple": isPlaintiff,
+              "text-darkPetrol": !isPlaintiff,
+            })}
+          >
+            Abbrechen
+          </Button>
+          <Button
+            onClick={() => setIsNewEntryVisible(false)}
+            size="sm"
+            bgColor="transparent"
+            textColor={cx("font-bold", {
+              "text-darkPurple": isPlaintiff,
+              "text-darkPetrol": !isPlaintiff,
+            })}
+          >
+            Speichern
+          </Button>
+        </div>
       </div>
     </div>
   );
