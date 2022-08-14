34987d2cdacf7b8e1b58330d2367f74a0095fbe9
Martina Emmert <tina.emmert@web.de>
2022-08-15 01:01:14 +0200

    #11 Add click actions to icons

--- src/components/SidebarHeader.tsx
+++ src/components/SidebarHeader.tsx
@@ -1,29 +1,55 @@
-import { Bookmarks, List, Notepad, Scales } from "phosphor-react"
+import { Bookmarks, CameraRotate, List, Notepad, Scales } from "phosphor-react"
+import { useState } from "react"
 import { Button } from "./Button"
 
 export const SidebarHeader = () => {
+
+    const [isNotesActive, setIsNotesActive] = useState<boolean>(false);
+    const [isHintsActive, setIsHintsActive] = useState<boolean>(false);
+    const [isBookmarksActive, setIsBookmarksActive] = useState<boolean>(false);
+    const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
+
     return (
-        <div className="flex flex-row justify-between ">
-            <Button bgColor="bg-offWhite"
-                size="sm"
-                textColor="font-bold text-darkGrey"
-                icon={<List size={16} />}>
-            </Button>
-            <div className="flex flex-row gap-2 w-[1h]">
-                <Button bgColor="bg-offWhite"
+        <div className={sidebarOpen ? "flex flex-row justify-between" : "flex flex-row justify-end"}>
+            <div className={sidebarOpen ? "transition duration-300 rotate-90" : "transition duration-300 rotate-0"}
+                onClick={() => setSidebarOpen(!sidebarOpen)}>
+                <Button bgColor="None"
                     size="sm"
                     textColor="font-bold text-darkGrey"
-                    icon={<Notepad size={16} />}>
+                    icon={<List size={18} />}>
                 </Button>
-                <Button bgColor="bg-offWhite"
+            </div>
+            
+            <div className={sidebarOpen ? "flex flex-row gap-2 w-[1h]" : "flex flex-row gap-2 w-[1h] hidden"}>
+                <Button bgColor={isNotesActive ? "bg-offWhite" : "None"}
                     size="sm"
                     textColor="font-bold text-darkGrey"
-                    icon={<Scales size={16} />}>
+                    icon={<Notepad size={18} />}
+                    onClick={() => {
+                        setIsNotesActive(true);
+                        setIsHintsActive(false);
+                        setIsBookmarksActive(false);
+                    }}>
                 </Button>
-                <Button bgColor="bg-offWhite"
+                <Button bgColor={isHintsActive ? "bg-offWhite" : "None"}
                     size="sm"
                     textColor="font-bold text-darkGrey"
-                    icon={<Bookmarks size={16} />}>
+                    icon={<Scales size={18} />}
+                    onClick={() => {
+                        setIsNotesActive(false);
+                        setIsHintsActive(true);
+                        setIsBookmarksActive(false);
+                    }}>
+                </Button>
+                <Button bgColor={isBookmarksActive ? "bg-offWhite" : "None"}
+                    size="sm"
+                    textColor="font-bold text-darkGrey"
+                    icon={<Bookmarks size={18} />}
+                    onClick={() => {
+                        setIsNotesActive(false);
+                        setIsHintsActive(false);
+                        setIsBookmarksActive(true);
+                    }}>
                 </Button>
             </div>
         </div>
