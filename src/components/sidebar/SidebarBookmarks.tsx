5c89d358663d0c487d308b4f954465086d347d6a
Martina Emmert <tina.emmert@web.de>
2022-08-19 11:43:44 +0200

    #10 Enhance info text for empty sidebar

--- src/components/sidebar/SidebarBookmarks.tsx
+++ src/components/sidebar/SidebarBookmarks.tsx
@@ -2,20 +2,20 @@ import { BookmarkSimple } from "phosphor-react";
 import { Bookmark, BookmarkProps } from "./Bookmark";
 
 //TODO: remove this, this is for testing
-const bookmarks: BookmarkProps[] = [
-  {
-    id: "1",
-    title: "Lesezeichen 1",
-    referenceTo: "12345",
-  },
-  {
-    id: "2",
-    title: "Lesezeichen 2",
-    referenceTo: "12345",
-  },
-];
+// const bookmarks: BookmarkProps[] = [
+//   {
+//     id: "1",
+//     title: "Lesezeichen 1",
+//     referenceTo: "12345",
+//   },
+//   {
+//     id: "2",
+//     title: "Lesezeichen 2",
+//     referenceTo: "12345",
+//   },
+// ];
 
-// const bookmarks: BookmarkProps[] = [];
+const bookmarks: BookmarkProps[] = [];
 
 export const SidebarBookmarks = () => {
   return (
@@ -24,13 +24,12 @@ export const SidebarBookmarks = () => {
         Lesezeichen
       </div>
       {bookmarks.length <= 0 && (
-        <div className="inline">
-          <span className="mt-7 text-darkGrey opacity-40 text-center text-sm inline-block">
-            Indem Sie auf das <BookmarkSimple size={14} className="inline" />
-            -Icon bei einem Beitrag klicken, wird dieser unter Ihren Lesezeichen
-            gespeichert. Ihre Lesezeichen erscheinen dann in dieser Ansicht.
-          </span>
-        </div>
+        <span className="mt-7 text-darkGrey opacity-40 text-center text-sm">
+          Indem Sie auf das <BookmarkSimple size={14} className="inline" />
+          -Icon bei einem Beitrag klicken, wird dieser unter Ihren Lesezeichen
+          gespeichert. Ihre Lesezeichen erscheinen dann in dieser Ansicht und
+          sind nur für Sie sichtbar.
+        </span>
       )}
       <div className="text-mediumGrey font-bold text-sm">
         {bookmarks.map((hint) => (
