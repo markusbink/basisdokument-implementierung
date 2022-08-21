b6c6f15a015d98718f22e7b44909dfafd48824d3
Martina Emmert <tina.emmert@web.de>
2022-08-21 18:44:46 +0200

    #4 Remove edit mode-later open popup to edit

--- src/components/sidebar/Note.tsx
+++ src/components/sidebar/Note.tsx
@@ -1,16 +1,7 @@
-import {
-  DotsThree,
-  Eye,
-  FloppyDisk,
-  PencilSimple,
-  Trash,
-  X,
-} from "phosphor-react";
+import { DotsThree, Eye, PencilSimple, Trash } from "phosphor-react";
 import { Button } from "../Button";
 import cx from "classnames";
 import React, { useRef, useState } from "react";
-import { Editor } from "react-draft-wysiwyg";
-import { ContentState, convertFromHTML, EditorState } from "draft-js";
 import { useOutsideClick } from "../../hooks/use-outside-click";
 
 export interface NoteProps {
@@ -22,59 +13,26 @@ export interface NoteProps {
   referenceTo?: string;
 }
 
-const toolbarOptions = {
-  options: ["inline", "list"],
-  inline: {
-    className: ["!mb-0 flex gap-2 items-center"],
-    options: ["bold", "italic", "underline", "strikethrough"],
-  },
-  list: {
-    className: ["!mb-0 flex gap-2 items-center"],
-    options: ["unordered", "ordered"],
-  },
-};
-
 export const Note: React.FC<NoteProps> = (note: NoteProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  const [editMode, setEditMode] = useState<boolean>(false);
-  const [editorState, setEditorState] = useState(() => {
-    const blocksFromHTML = convertFromHTML(note.content || "");
-    const contentState = ContentState.createFromBlockArray(
-      blocksFromHTML.contentBlocks,
-      blocksFromHTML.entityMap
-    );
-
-    return EditorState.createWithContent(contentState);
-  });
   const ref = useRef(null);
   useOutsideClick(ref, () => setIsMenuOpen(false));
 
   const showReference = (e: React.MouseEvent) => {
-    //TODO
+    //TODO jump to reference
   };
 
   const editNote = (e: React.MouseEvent) => {
     setIsMenuOpen(false);
-    setEditMode(!editMode);
-    //TODO
+    //TODO open popup
   };
 
   const deleteNote = (e: React.MouseEvent) => {
     //TODO
   };
 
-  const onAbort = () => {
-    setEditMode(!editMode);
-    //TODO
-  };
-
-  const onSave = () => {
-    setEditMode(!editMode);
-    //TODO
-  };
-
   return (
-    <div ref={ref}>
+    <div>
       <div className="flex flex-col bg-offWhite mt-4 rounded-xl text-darkGrey text-xs font-medium">
         {note.referenceTo && (
           <div
@@ -89,50 +47,7 @@ export const Note: React.FC<NoteProps> = (note: NoteProps) => {
 
         <div className={cx("mx-3", { "mt-3": !note.referenceTo })}>
           <div className="mb-2 text-sm font-bold">{note.title}</div>
-          {editMode ? (
-            <div>
-              <Editor
-                defaultEditorState={editorState}
-                onEditorStateChange={setEditorState}
-                wrapperClassName={cx(
-                  "min-h-[140px] w-full focus:outline-none bg-white rounded-t-lg"
-                )}
-                editorClassName="p-2"
-                placeholder="Text eingeben..."
-                toolbarClassName={cx(
-                  "p-2 relative rounded-none border border-x-0 border-t-0 border-lightGrey flex gap-5"
-                )}
-                toolbar={toolbarOptions}
-                toolbarCustomButtons={[]}
-              />
-              <div className="flex justify-end gap-1 p-2 border-t border-lightGrey bg-white rounded-b-lg">
-                <Button
-                  icon={<X size={16} />}
-                  onClick={() => onAbort()}
-                  size="xs"
-                  gap="gap-0.5"
-                  bgColor="bg-lightRed"
-                  alternativePadding="p-1"
-                  textColor="font-bold text-darkRed"
-                >
-                  Abbrechen
-                </Button>
-                <Button
-                  icon={<FloppyDisk size={16} />}
-                  onClick={() => onSave()}
-                  size="xs"
-                  gap="gap-0.5"
-                  bgColor="bg-lightGreen"
-                  alternativePadding="p-1"
-                  textColor="font-bold text-darkGreen"
-                >
-                  Speichern
-                </Button>
-              </div>
-            </div>
-          ) : (
-            <div className="mb-2">{note.content}</div>
-          )}
+          <div className="mb-2">{note.content}</div>
 
           <div className="flex justify-between items-center mb-3">
             <div className="">
@@ -144,7 +59,7 @@ export const Note: React.FC<NoteProps> = (note: NoteProps) => {
               </div>
             </div>
 
-            <div className="self-end relative">
+            <div ref={ref} className="self-end relative">
               <Button
                 key="createNote"
                 bgColor={
