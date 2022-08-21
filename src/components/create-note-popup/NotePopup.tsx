9b8607364bc815b7a56ae3afd51f3ce9c20f36f4
MichelleLanzinger <42041831+MichelleLanzinger@users.noreply.github.com>
2022-08-21 11:36:02 +0200

    5: Fixed suggested changes

--- src/components/create-note-popup/NotePopup.tsx
+++ src/components/create-note-popup/NotePopup.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import {XCircle, WarningCircle, Quotes} from "phosphor-react";
 import { Button } from "../Button";
 import cx from "classnames";
 import { ContentState, convertFromHTML, EditorState } from "draft-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
@@ -25,8 +24,9 @@ const toolbarOptions = {
 
 // TODO: Exchange the button that opens the modal with the actual button
 
-export default function Note() {
-    const [showModal, setShowModal] = React.useState(false);
+export const NotePopup = () => {
+    const [hidePlaceholder, setHidePlaceholder] = useState<boolean>(false);
+    const [showModal, setShowModal] = useState(false);
     const [editorState, setEditorState] = useState(() => {
       const blocksFromHTML = convertFromHTML("");
       const contentState = ContentState.createFromBlockArray(
@@ -35,6 +35,15 @@ export default function Note() {
       );
       return EditorState.createWithContent(contentState);
     });
+
+    const contentState = editorState.getCurrentContent();
+
+    useEffect(() => {
+      setHidePlaceholder(
+        () => contentState.getBlockMap().first().getType() !== "unstyled"
+      );
+    }, [contentState]);
+    
     return (
       <>
         <button
@@ -47,41 +56,43 @@ export default function Note() {
         {showModal ? (
           <>
             <div
-              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+              className={cx("justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none", {
+                "RichEditor-hidePlaceholder": hidePlaceholder,
+              })}
             >
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="w-auto my-6 mx-auto max-w-3xl">
                 {/*content*/}
-                <div className="p-2 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="p-2 border-0 rounded-lg shadow-lg flex flex-col w-full bg-white outline-none focus:outline-none">
                   {/*header*/}
-                  <div className="flex items-start justify-between rounded-lg">
+                  <div className="flex items-start justify-between rounded-lg ">
                     <h3 className="text-2xl font-bold text-darkGrey pb-6 pt-4 pl-3">
                       Neue Notiz verfassen
                     </h3>
                     <div>
-                    <Button icon={<XCircle size={29} color="#3A4342" weight="fill"/>}
-                      onClick={() => setShowModal(false)} 
-                      bgColor="transparent"
-                      >
-                    </Button>
+                    <button onClick={() => setShowModal(false)} className="text-darkGrey">
+                      <XCircle size={29} weight="fill" />
+                    </button>
                     </div>
                   </div>
-                  <div className="flex space-x-4 mx-20 p-3 m-1 items-center justify-center bg-lightOrange rounded-lg text-xs font-bold text-darkOrange">
-                    <div>
-                        <WarningCircle> icon={<WarningCircle size={50} color="darkOrange" weight="bold" />}</WarningCircle>
-                    </div>
-                    <p>
-                      Alle Notizen sind privat und können nicht von den anderen Parteien eingesehen werden. 
-                      Sie können die Notitz zu jedem Zeitpunkt bearbeiten und löschen.
-                    </p>
-                  </div>
+                  <div className="flex gap-2 mx-20 p-3 bg-lightOrange rounded-lg font-bold text-darkOrange">
+                  <span>
+                    <WarningCircle size={40} />
+                  </span>
+                  <p className="text-sm">
+                    Alle Notizen sind privat und können nicht von den anderen
+                    Parteien eingesehen werden. Sie können die Notitz zu jedem
+                    Zeitpunkt bearbeiten und löschen.
+                  </p>
+                </div>
                   {/*body*/}
-                  <div className="relative p-3 space-y-2">
-                    <label className="text-slate-500 leading-relaxed text-lg font-bold text-darkGrey">
+                  <div className="p-3 space-y-4">
+                  <div className="space-y-2">
+                    <label htmlFor="note_popup_input" className="text-slate-500 leading-relaxed text-lg font-bold text-darkGrey">
                       Titel
                     </label>
-                    <input className="w-full p-1 bg-offWhite block rounded text-mediumGrey focus:outline-none" placeholder="Titel eingeben..."></input>
+                    <input id="note_popup_input" className="w-full px-2 py-3 bg-offWhite block rounded text-mediumGrey focus:outline-none" placeholder="Titel eingeben..."></input>
                   </div>
-                  <div className="relative p-3">
+                  <div>
                     <label className="my-4 text-slate-500 text-lg leading-relaxed font-bold text-darkGrey">
                       Text
                     </label>
@@ -89,24 +100,25 @@ export default function Note() {
                       defaultEditorState={editorState}
                       onEditorStateChange={setEditorState}
                       wrapperClassName={cx("min-h-[140px] w-full focus:outline-none")}
-                      editorClassName="pb-8 px-1 bg-offWhite text-mediumGrey rounded"
+                      editorClassName="px-1 bg-offWhite text-mediumGrey rounded"
                       placeholder="Text eingeben..."
                       toolbarClassName={cx("p-2 relative rounded-none border-white")}
                       toolbar={toolbarOptions}
                     />
+                  </div>
                   </div>
                   {/*footer*/}
                   <div className="p-3 space-y-2">
                     <label className="my-4 text-slate-500 text-lg leading-relaxed font-bold text-darkGrey">
                       Verweis auf einen Beitrag hinzufügen
                     </label>
-                    <div className="flex p-3 items-center justify-content w-fit rounded-lg bg-offWhite text-darkGrey text-xs">
-                      <p className="flex-2 pr-3">Beitrag auswählen:</p>
+                    <div className="flex p-3 items-center justify-content w-fit rounded-lg bg-offWhite text-darkGrey gap-3">
+                      <p className="text-sm">Beitrag auswählen:</p>
                       <div className="flex flex-1 border-lightGrey border border-solid p-1 rounded-lg">
                         <div className="bg-darkGrey rounded flex items-center p-1 m-1">
                           <Quotes size={16} color="white" weight="regular" />
                         </div>
-                        <div className="flex items-center gap-2 cursor-pointer relative rounded-full pl-3 pr-1 py-1 m-1 text-xs font-semibold bg-darkGrey text-white">
+                        <div className="flex items-center gap-2 cursor-pointer rounded-full pl-3 pr-1 py-1 m-1 text-xs font-semibold bg-darkGrey text-white">
                           K-2-1
                           <XCircle size={20} weight="fill" />
                         </div>
@@ -114,13 +126,11 @@ export default function Note() {
                     </div>
                   </div>
                   <div className="flex items-center justify-end p-6 rounded-b">
-                    <button
-                      className="bg-darkGrey text-white font-bold text-xs p-3 rounded-lg outline-none"
-                      type="button"
+                    <Button
                       onClick={() => setShowModal(false)}
                     >
                       Notiz hinzufügen
-                    </button>
+                    </Button>
                   </div>
                 </div>
               </div>
