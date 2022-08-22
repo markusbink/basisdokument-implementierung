f7dd6ab907546c120058c87c5f314d3f6965e8ca
Tien Julian Ho <53001076+Tien1207@users.noreply.github.com>
2022-08-23 00:28:59 +0200

    #15 Split into Grid

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -1,5 +1,71 @@
+import { Button } from "./Button"
+import { DotsThree, Notepad} from "phosphor-react";
+
 export const Meta = () => {
     return (
-        <div></div>
+        <div className="bg-offWhite h-full overflow-y-scroll p-4 space-y-4">
+            <div className="max-w-[1200px] m-auto"></div>
+                <div className="grid grid-cols-2 gap-4">
+                    <div className="col-start-1 col-span-1 ">Klägerpartei</div>
+                    <div className="bg-lightPurple text-black p-4 rounded-lg col-start-1 col-span-1">
+                        <div className="flex justify-between">
+                            <h3 className="font-bold">Kurt Huber</h3>
+                            <span className="inline-flex">
+                                <Button
+                                    bgColor="transparent"
+                                    textColor="text-darkGrey"
+                                    icon={<Notepad size={18} />}
+                                    alternativePadding="p-1"
+                                />
+                                <Button 
+                                    bgColor="transparent"
+                                    textColor="text-darkGrey"
+                                    icon={<DotsThree size={18} />}
+                                    alternativePadding="p-1"
+                                />
+                            </span>
+                        </div>
+                        <p className="mt-8">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Consequuntur dolorum earum dolores omnis odit, voluptas
+                            ratione? Praesentium reprehenderit perspiciatis repudiandae
+                            officia veniam qui facere at deserunt, harum ab pariatur
+                            beatae?
+                        </p>
+                    </div>
+                    <div className="col-start-2 col-span-1 row-start-1 row-span-1">
+                        <Button
+                            bgColor="lightPetrol"
+                            textColor="text-darkGrey" />
+                             Beklagtenpartei
+                            </div>
+                    <div className="bg-lightPetrol text-black p-4 rounded-lg col-start-2 col-span-1">
+                        <div className="flex justify-between">
+                            <h3 className="font-bold">Kurt Huber</h3>
+                            <span className="inline-flex">
+                                    <Button
+                                        bgColor="transparent"
+                                        textColor="text-darkGrey"
+                                        icon={<Notepad size={18} />}
+                                        alternativePadding="p-1"
+                                    />
+                                    <Button 
+                                        bgColor="transparent"
+                                        textColor="text-darkGrey"
+                                        icon={<DotsThree size={18} />}
+                                        alternativePadding="p-1"
+                                    />
+                            </span>
+                    </div>
+                        <p className="mt-8">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Consequuntur dolorum earum dolores omnis odit, voluptas
+                            ratione? Praesentium reprehenderit perspiciatis repudiandae
+                            officia veniam qui facere at deserunt, harum ab pariatur
+                            beatae?
+                        </p>
+                    </div>
+                </div>
+        </div>
     )
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,14 @@
 import { Discussion } from "../components/Discussion";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/sidebar/Sidebar";
+import { Meta } from "../components/Meta";
 
 export const Main: React.FC = () => {
   return (
     <div className="flex w-full h-full">
       <main className="w-full flex flex-col">
         <Header />
+        <Meta />
         <Discussion />
       </main>
       <Sidebar />
