bb9cb0ba92c3720dcc5130418723b38cd876b53e
NilsHellwig <nc.hellwig@gmail.com>
2022-08-24 19:46:40 +0200

    white circle for markers

--- src/components/header/HighlighterButton.tsx
+++ src/components/header/HighlighterButton.tsx
@@ -4,14 +4,13 @@ import cx from "classnames";
 import { useHeaderContext } from "../../contexts";
 
 export const HighlighterButton: React.FC<{ id: number }> = ({ id }) => {
-  const { colorSelection, highlighterData, setHighlighterData} = useHeaderContext();
-  const color:string = colorSelection[id].id;
-  
+  const { colorSelection, highlighterData, setHighlighterData } = useHeaderContext();
+  const color: string = colorSelection[id].id;
 
   return (
     <div
       className={cx(`marker-${colorSelection[id].id} flex justify-center items-center text-white h-5 w-5 rounded-full hover:border-2 cursor-pointer hover:border-darkGrey`, {
-        "opacity-50": !highlighterData[color],
+        "opacity-40": !highlighterData[color],
       })}
       onClick={() => {
         setHighlighterData({
@@ -20,7 +19,9 @@ export const HighlighterButton: React.FC<{ id: number }> = ({ id }) => {
         });
       }}
     >
-      {highlighterData[color] ? <Check size={14} weight="bold" className="text-darkGrey" /> : null}
+      {highlighterData[color] ? (
+        <div className="border-white w-4 h-4 bg-transparent rounded-full bg-transparent border-[2px]" />
+      ) : null}
     </div>
   );
 };
