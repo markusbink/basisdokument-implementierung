d984eb4bc6f4bc5967ff1f3a486ed851fd045870
markusbink <bink-markus@web.de>
2022-08-25 12:41:51 +0200

    #18 Make Popup reusable

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,6 @@
 import { Discussion } from "../components/Discussion";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
-import { Popup } from "../components/Popup";
 
 export const Main: React.FC = () => {
   return (
@@ -9,7 +8,6 @@ export const Main: React.FC = () => {
       <main className="w-full flex flex-col">
         <Header />
         <Discussion />
-        <Popup />
       </main>
       <Sidebar />
     </div>
