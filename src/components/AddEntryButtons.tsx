0b3d0c8e465248036543dbf728d151209088b8e2
markusbink <bink-markus@web.de>
2022-08-27 20:04:14 +0200

    #68 Fix button display based on role

--- src/components/AddEntryButtons.tsx
+++ src/components/AddEntryButtons.tsx
@@ -1,5 +1,6 @@
 import { Plus } from "phosphor-react";
 import { useState } from "react";
+import { useUser } from "../contexts";
 import { UserRole } from "../types";
 import { Button } from "./Button";
 import { NewEntry } from "./entry";
@@ -15,6 +16,7 @@ export const AddEntryButtons: React.FC<AddEntryButtonsProps> = ({
   const [newEntryRole, setNewEntryRole] = useState<"Kläger" | "Beklagter">(
     "Kläger"
   );
+  const { user } = useUser();
 
   const handleClick = (roleForNewEntry: "Kläger" | "Beklagter") => {
     setNewEntryRole(roleForNewEntry);
@@ -31,22 +33,28 @@ export const AddEntryButtons: React.FC<AddEntryButtonsProps> = ({
         />
       ) : (
         <div className="grid grid-cols-2 gap-6 mb-8 items-start w-full">
-          <span className="col-start-1">
-            <Button
-              onClick={() => handleClick(UserRole.Plaintiff)}
-              icon={<Plus size={18} weight="bold" />}
-            >
-              Beitrag hinzufügen
-            </Button>
-          </span>
-          <span className="col-start-2">
-            <Button
-              onClick={() => handleClick(UserRole.Defendant)}
-              icon={<Plus size={18} weight="bold" />}
-            >
-              Beitrag hinzufügen
-            </Button>
-          </span>
+          <div>
+            {(user?.role === UserRole.Plaintiff ||
+              user?.role === UserRole.Judge) && (
+              <Button
+                onClick={() => handleClick(UserRole.Plaintiff)}
+                icon={<Plus size={18} weight="bold" />}
+              >
+                Beitrag hinzufügen
+              </Button>
+            )}
+          </div>
+          <div>
+            {(user?.role === UserRole.Defendant ||
+              user?.role === UserRole.Judge) && (
+              <Button
+                onClick={() => handleClick(UserRole.Defendant)}
+                icon={<Plus size={18} weight="bold" />}
+              >
+                Beitrag hinzufügen
+              </Button>
+            )}
+          </div>
         </div>
       )}
     </>
