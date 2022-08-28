5db4491df1ac07892c137b47b8c3ec525414de9f
NilsHellwig <nc.hellwig@gmail.com>
2022-08-28 09:45:01 +0200

    do not lower opacity when editing

--- src/components/entry/Entry.tsx
+++ src/components/entry/Entry.tsx
@@ -142,7 +142,7 @@ export const Entry: React.FC<EntryProps> = ({
         id={entry.entryCode}
         className={cx("text-sm", {
           "opacity-50": isHidden,
-          "opacity-30 pointer-events-none":!lowerOpacityForSearch && searchbarValue !== "",
+          "opacity-30 pointer-events-none":!lowerOpacityForSearch && searchbarValue !== "" && !isEditing,
           "pointer-events-none": isHidden,
         })}
       >
