6b414f99a73d45cb5242012ad9f2353f35c945a4
NilsHellwig <nc.hellwig@gmail.com>
2022-08-30 09:26:31 +0200

    show warning when user tries to close tab

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,8 +2,10 @@ import { Discussion } from "../components/Discussion";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/sidebar/Sidebar";
 import { Onboarding } from "../components/Onboarding";
+import { useBeforeunload } from 'react-beforeunload';
 
 export const Main: React.FC = () => {
+  useBeforeunload(() => "Änderungen am Basisdokument können verloren gehen. Bitte speichern Sie das Basisdokument, bevor Sie den Browser-Tab schließen.");
   return (
     <div className="flex w-full h-full">
       <Onboarding />
