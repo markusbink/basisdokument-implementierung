419b24fc279a9f555f3b7aa621bc6a06c816f095
Martina Emmert <tina.emmert@web.de>
2022-09-04 17:03:11 +0200

    #106 Add opacity if not own entry referenced

--- src/components/sidebar/Hint.tsx
+++ src/components/sidebar/Hint.tsx
@@ -1,9 +1,10 @@
 import cx from "classnames";
+import { userInfo } from "os";
 import { DotsThree, Eye, PencilSimple, Trash } from "phosphor-react";
 import { useRef, useState } from "react";
-import { useCase } from "../../contexts";
+import { useCase, useUser } from "../../contexts";
 import { useOutsideClick } from "../../hooks/use-outside-click";
-import { IHint } from "../../types";
+import { IHint, UserRole } from "../../types";
 import { getEntryCode } from "../../util/get-entry-code";
 import { Button } from "../Button";
 
@@ -17,6 +18,7 @@ export const Hint: React.FC<HintProps> = ({ hint }) => {
   useOutsideClick(ref, () => setIsMenuOpen(false));
   const { entries } = useCase();
   const entryCode = getEntryCode(entries, hint.associatedEntry);
+  const { user } = useUser();
 
   const editHint = (e: React.MouseEvent) => {
     setIsMenuOpen(false);
@@ -27,15 +29,29 @@ export const Hint: React.FC<HintProps> = ({ hint }) => {
     //TODO
   };
 
+  console.log(hint.associatedEntry);
+  console.log(user?.role);
+  console.log(UserRole.Judge);
+  console.log(user?.role !== UserRole.Judge);
+  console.log(entryCode);
+  console.log(entryCode.charAt(0) !== user?.role.charAt(0));
+
   return (
     <div>
-      <div className="flex flex-col bg-offWhite mt-4 rounded-xl text-darkGrey text-xs font-medium">
+      <div
+        className={cx(
+          "flex flex-col bg-offWhite mt-4 rounded-xl text-darkGrey text-xs font-medium",
+          {
+            "opacity-50":
+              hint.associatedEntry &&
+              user?.role !== UserRole.Judge &&
+              entryCode.charAt(0) !== user?.role.charAt(0),
+          }
+        )}>
         {hint.associatedEntry && (
           <a
             href={`#${entryCode}`}
-            className="flex gap-1 mt-1.5 mr-1.5 px-1.5 py-0.5 self-end w-fit cursor-pointer
-              bg-darkGrey hover:bg-mediumGrey text-lightGrey text-[10px] font-semibold rounded-xl"
-          >
+            className="flex gap-1 mt-1.5 mr-1.5 px-1.5 py-0.5 self-end w-fit cursor-pointer bg-darkGrey hover:bg-mediumGrey text-lightGrey text-[10px] font-semibold rounded-xl">
             <Eye size={16} weight="bold" className="inline"></Eye>
             {`${entryCode}`}
           </a>
@@ -69,23 +85,20 @@ export const Hint: React.FC<HintProps> = ({ hint }) => {
                 onClick={() => {
                   setIsMenuOpen(!isMenuOpen);
                 }}
-                icon={<DotsThree size={20} weight="bold" />}
-              ></Button>{" "}
+                icon={<DotsThree size={20} weight="bold" />}></Button>{" "}
               {isMenuOpen ? (
                 <ul className="absolute right-0 bottom-8 p-2 bg-white text-darkGrey rounded-xl w-[150px] shadow-lg z-50 font-medium">
                   <li
                     tabIndex={0}
                     onClick={editHint}
-                    className="flex items-center gap-2 p-2 rounded-lg hover:bg-offWhite focus:bg-offWhite focus:outline-none"
-                  >
+                    className="flex items-center gap-2 p-2 rounded-lg hover:bg-offWhite focus:bg-offWhite focus:outline-none">
                     <PencilSimple size={16} />
                     Bearbeiten
                   </li>
                   <li
                     tabIndex={0}
                     onClick={deleteHint}
-                    className="flex items-center gap-2 p-2 rounded-lg text-vibrantRed hover:bg-offWhite focus:bg-offWhite focus:outline-none"
-                  >
+                    className="flex items-center gap-2 p-2 rounded-lg text-vibrantRed hover:bg-offWhite focus:bg-offWhite focus:outline-none">
                     <Trash size={16} />
                     Löschen
                   </li>
