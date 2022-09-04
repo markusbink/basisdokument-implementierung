d604501f1ea76197bb3fccbac533c9c4cf2fbe4b
NilsHellwig <nc.hellwig@gmail.com>
2022-09-04 19:21:17 +0200

    get entries by useCase

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,31 +1,55 @@
 import { Quotes } from "phosphor-react";
 import React, { useState } from "react";
-import { useNotes } from "../contexts";
+import { useCase, useNotes } from "../contexts";
+import { IEntry } from "../types";
 
-export const Dropdown = ({ options }: {options:any}) => {
-    const [showOptions, setShowOptions] = useState(false);
-    const { setAssociatedEntryId} = useNotes();
-    const handleClick = () => {
-        setShowOptions(!showOptions);
-    }
-    return(
-        <div className="bg-darkGrey rounded flex items-center p-1 m-1 ">
-        <div className="relative inline-block text-left">
+export const Dropdown = ({ options }: { options: any }) => {
+  const [showOptions, setShowOptions] = useState(false);
+  const { setAssociatedEntryId } = useNotes();
+  const { entries } = useCase();
+  const handleClick = () => {
+    setShowOptions(!showOptions);
+  };
+  return (
+    <div className="bg-darkGrey rounded flex items-center p-1 m-1 ">
+      <div className="relative inline-block text-left">
         <div>
-          <button 
-          onClick={handleClick}
-          type="button" className="" id="menu-button" aria-expanded="true" aria-haspopup="true">
+          <button
+            onClick={handleClick}
+            type="button"
+            className=""
+            id="menu-button"
+            aria-expanded="true"
+            aria-haspopup="true">
             <Quotes size={16} color="white" weight="regular" />
           </button>
         </div>
-        {showOptions && (<div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
-          <div className="py-1" role="none">
-            {options && options.map((option:string) => (
-            <button  onClick={() => setAssociatedEntryId(option)} className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" id="menu-item-0">{option}</button>)) }
+        {showOptions && (
+          <div
+            className="absolute flex flex-col w-max right-0 z-10 mt-4 p-2 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="menu-button">
+            <div className="py-1" role="none">
+              {entries &&
+                entries.map((entry: IEntry) => (
+                  <button
+                    key={entry.id}
+                    onClick={() => {
+                      console.log(entry.id);
+                      
+                      setAssociatedEntryId(entry.id);
+                    }}
+                    className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 w-full rounded-md"
+                    role="menuitem"
+                    id="menu-item-0">
+                    {entry.entryCode}
+                  </button>
+                ))}
+            </div>
           </div>
-        </div>)}
+        )}
       </div>
-      </div>
-    );
-}
-
+    </div>
+  );
+};
