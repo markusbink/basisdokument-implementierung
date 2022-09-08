f97d69fda1af268558eb049bdfde2599ac966292
Nicole Schönwerth <40839111+NiciSc@users.noreply.github.com>
2022-09-08 11:54:57 +0200

    #65 Update Styling

--- src/components/Onboarding.tsx
+++ src/components/Onboarding.tsx
@@ -22,7 +22,7 @@ export const Onboarding = () => {
         <div className="z-30 fixed">
           <div className="w-screen h-screen inset-0 fixed opacity-75 bg-darkGrey" />
           <div className="w-screen h-screen inset-0 fixed flex items-center justify-center p-4">
-            <div className="relative bg-white max-w-5xl w-auto rounded-xl max-h-[750px] h-full overflow-hidden">
+            <div className="relative bg-white max-w-5xl w-auto rounded-xl overflow-hidden max-h-[750px] h-full m-2">
               <button
                 className="absolute top-0 right-0 p-3 select-all z-10"
                 onClick={toggleOnboarding}
