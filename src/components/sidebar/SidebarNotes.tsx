80463a6a121a8ba0d1885a8b074d4626a57004b9
Martina Emmert <tina.emmert@web.de>
2022-09-09 16:23:35 +0200

    #124 Fix scrollbar sidebar

--- src/components/sidebar/SidebarNotes.tsx
+++ src/components/sidebar/SidebarNotes.tsx
@@ -15,7 +15,7 @@ export const SidebarNotes = () => {
 
   return (
     <div>
-      <div className="flex flex-col gap-3 flex-1 overflow-hidden">
+      <div className="flex flex-col gap-3 h-[calc(100vh_-_3.5rem)] overflow-auto">
         <div className="flex justify-between items-center pt-4 px-4">
           <div className="font-bold text-darkGrey text-lg">Notizen</div>
           <Button
@@ -31,25 +31,25 @@ export const SidebarNotes = () => {
             icon={<Plus size={18} weight="bold" />}></Button>
         </div>
 
-        {notes.length <= 0 && (
+        {notes.length <= 0 ? (
           <div className="mt-7 text-darkGrey opacity-40 text-center text-sm p-4">
             Notizen, die Sie zu Beiträgen verfassen, erscheinen in dieser
             Ansicht und sind nur für Sie sichtbar.
           </div>
-        )}
-
-        {notes.length > 0 && (
-          <div className="flex flex-col p-4 text-mediumGrey font-extrabold text-sm overflow-auto">
+        ) : (
+          <div className="flex flex-col p-4 text-mediumGrey font-extrabold text-sm h-fit">
             <div
               className="cursor-pointer flex items-center"
               onClick={() =>
                 setNotesWithoutReferenceOpen(!notesWithoutReferenceOpen)
               }>
-              {notesWithoutReferenceOpen ? (
-                <CaretDown size={14} className="inline mr-1" weight="bold" />
-              ) : (
-                <CaretRight size={14} className="inline mr-1" weight="bold" />
-              )}
+              <div>
+                {notesWithoutReferenceOpen ? (
+                  <CaretDown size={14} className="inline mr-1" weight="bold" />
+                ) : (
+                  <CaretRight size={14} className="inline mr-1" weight="bold" />
+                )}
+              </div>
               OHNE BEZUG AUF BEITRAG
             </div>
             <div>
