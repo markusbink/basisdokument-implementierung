d13ad7d5f9f35a3bedc4e5f823e58cb99491fb76
Martina Emmert <tina.emmert@web.de>
2022-09-09 18:45:37 +0200

    #126 Add Button reply

--- src/components/AddEntryButtons.tsx
+++ src/components/AddEntryButtons.tsx
@@ -4,6 +4,7 @@ import { useUser } from "../contexts";
 import { UserRole } from "../types";
 import { Button } from "./Button";
 import { NewEntry } from "./entry";
+import cx from "classnames";
 
 interface AddEntryButtonsProps {
   sectionId: string;
