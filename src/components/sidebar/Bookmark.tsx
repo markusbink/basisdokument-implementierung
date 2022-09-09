304fe573579e72dba7237f15ea08b6c5891351ef
Martina Emmert <tina.emmert@web.de>
2022-09-09 15:29:49 +0200

    #124 Fix pile color

--- src/components/sidebar/Bookmark.tsx
+++ src/components/sidebar/Bookmark.tsx
@@ -5,6 +5,7 @@ import { IBookmark } from "../../types";
 import { getEntryCode } from "../../util/get-entry-code";
 import { Button } from "../Button";
 import { Tooltip } from "../Tooltip";
+import cx from "classnames";
 
 export interface BookmarkProps {
   bookmark: IBookmark;
@@ -40,74 +41,79 @@ export const Bookmark: React.FC<BookmarkProps> = ({ bookmark }) => {
 
   return (
     <div className="flex justify-between gap-2 items-center bg-offWhite rounded-lg mb-2 p-2 font-medium">
-      <div className="flex flex-row items-center gap-2">
-        <div>
-          <BookmarkSimple size={20} weight="fill" className="text-darkGrey" />
-        </div>
-        <div className="flex flex-row justify-start w-full gap-2">
-          {bookmark.isInEditMode ? (
-            <input
-              autoFocus={true}
-              type="text"
-              name="title"
-              placeholder="Titel vergeben..."
-              maxLength={40}
-              className="focus:outline focus:outline-offWhite focus:bg-offWhite p-0 m-0"
-              value={bookmark.title}
-              onBlur={() => setBookmarkEditMode(bookmark, false)}
-              onChange={handleChange}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  setBookmarkEditMode(bookmark, false);
-                }
-              }}
-            />
-          ) : (
-           
-            <Tooltip text="Doppelklick, um zu Editieren">
-            <div
-              className="break-words text-left"
-              onDoubleClick={() => {
-                setBookmarkEditMode(bookmark, true);
-              }}>
-              {bookmark.title}
-            </div>
-          </Tooltip>
-     
-          )}
-        </div>
-      </div>
-
       <div>
-        <div className="flex items-center gap-2">
-          {entryCode ? (
-            <a
-              href={`#${entryCode}`}
-              className="flex items-center gap-1 px-1.5 py-0.25 rounded-xl bg-darkGrey hover:bg-mediumGrey
-          text-lightGrey text-[10px] font-semibold cursor-pointer min-w-fit">
-              <Eye size={16} weight="bold" className="inline"></Eye>
-              {`${entryCode}`}
-            </a>
-          ) : (
-            <div
-              className="flex items-center gap-1 px-1.5 py-0.25 rounded-xl bg-darkGrey
-          text-lightGrey text-[9px] font-semibold min-w-fit">
-              <Eye size={12} weight="bold" className="inline"></Eye>
-              {`fehlend`}
-            </div>
-          )}
+        <BookmarkSimple
+          size={20}
+          weight="fill"
+          className="text-darkGrey w-fit"
+        />
+      </div>
+      <div className="flex flex-grow w-auto overflow-hidden">
+        {bookmark.isInEditMode ? (
+          <input
+            autoFocus={true}
+            type="text"
+            name="title"
+            placeholder="Titel vergeben..."
+            maxLength={40}
+            className="focus:outline focus:outline-offWhite focus:bg-offWhite p-0 m-0"
+            value={bookmark.title}
+            onBlur={() => setBookmarkEditMode(bookmark, false)}
+            onChange={handleChange}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setBookmarkEditMode(bookmark, false);
+              }
+            }}
+          />
+        ) : (
+          // <Tooltip text="Doppelklick, um zu Editieren">
+          <div
+            className="break-words w-full"
+            onDoubleClick={() => {
+              setBookmarkEditMode(bookmark, true);
+            }}>
+            {bookmark.title}
+          </div>
+          // </Tooltip>
+        )}
+      </div>
+      <div className="flex items-center gap-2">
+        {entryCode ? (
+          <a
+            href={`#${entryCode}`}
+            className={cx(
+              "flex items-center gap-1 px-1.5 py-0.25 rounded-xl text-[10px] font-semibold cursor-pointer min-w-fit grow",
+              {
+                "bg-darkGrey text-offWhite hover:bg-mediumGrey": !entryCode,
+                "bg-lightPurple text-darkPurple hover:bg-darkPurple hover:text-lightPurple":
+                  entryCode?.charAt(0) === "K",
+                "bg-lightPetrol text-darkPetrol hover:bg-darkPetrol hover:text-lightPetrol":
+                  entryCode?.charAt(0) === "B",
+              }
+            )}>
+            <Eye size={16} weight="bold" className="inline"></Eye>
+            {`${entryCode}`}
+          </a>
+        ) : (
+          <div
+            className="flex items-center gap-1 px-1.5 py-0.25 rounded-xl bg-darkGrey
+          text-lightGrey text-[9px] font-semibold min-w-fit grow">
+            <Eye size={12} weight="bold" className="inline"></Eye>
+            {`fehlend`}
+          </div>
+        )}
 
-
-          <Button
-            key="createNote"
-            bgColor="bg-lightRed hover:bg-marker-red"
-            size="sm"
-            textColor="text-darkRed"
-            hasText={false}
-            alternativePadding="p-1"
-            onClick={deleteBookmark}
-            icon={<Trash size={16} />}></Button>
-        </div>
+        <Button
+          key="deleteNote"
+          bgColor="bg-lightRed hover:bg-marker-red relative"
+          size="sm"
+          textColor="text-darkRed"
+          hasText={false}
+          alternativePadding="p-1"
+          onClick={deleteBookmark}
+          icon={<Trash size={16} />}
+        />
       </div>
     </div>
   );
