0f37e4f668bdce1b9c12ef604692945bd9b65807
Martina Emmert <tina.emmert@web.de>
2022-09-10 12:50:46 +0200

    #138 Add ErrorPopup for section

--- src/components/section-header/SectionDropdown.tsx
+++ src/components/section-header/SectionDropdown.tsx
@@ -4,6 +4,8 @@ import { DotsThree, Trash, CheckCircle, XCircle, Circle } from "phosphor-react";
 import { useCase, useSection, useUser } from "../../contexts";
 import { UserRole } from "../../types";
 import { Tooltip } from "../Tooltip";
+import { ErrorPopup } from "../ErrorPopup";
+import { useState } from "react";
 
 interface SectionDropdownProps {
   sectionId: string;
@@ -17,6 +19,8 @@ export const SectionDropdown: React.FC<SectionDropdownProps> = ({
   const { user } = useUser();
   const { entries, currentVersion, setLitigiousChecks } = useCase();
   const { setSectionList } = useSection();
+  const [isDeleteErrorVisible, setIsDeleteErrorVisible] =
+    useState<boolean>(false);
 
   const isOld = version < currentVersion;
   const sectionEntries = entries.filter(
@@ -81,50 +85,80 @@ export const SectionDropdown: React.FC<SectionDropdownProps> = ({
   };
 
   return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger className="py-1">
-        <span>
-          <Tooltip text="Mehr Optionen">
-            <DotsThree
-              size={20}
-              weight="bold"
-              className="rounded-md hover:bg-darkPurple hover:text-lightPurple"
-            />
-          </Tooltip>
-        </span>
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content
-          align="end"
-          className="bg-darkGrey rounded-lg shadow-lg z-50">
-          {!isOld && (
-            <DropdownMenu.Item onClick={() => deleteSection()}>
-              <Button icon={<Trash size={18} />} size="sm">
-                Gliederungspunkt löschen
-              </Button>
-            </DropdownMenu.Item>
-          )}
-          {user?.role === UserRole.Judge && (
-            <>
-              <DropdownMenu.Item onClick={() => resetLitigiousChecks()}>
-                <Button icon={<Circle size={18} />} size="sm">
-                  Alle Streitigkeitsmarkierungen zurücksetzen
+    <>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger className="py-1">
+          <span>
+            <Tooltip text="Mehr Optionen">
+              <DotsThree
+                size={20}
+                weight="bold"
+                className="rounded-md hover:bg-darkPurple hover:text-lightPurple"
+              />
+            </Tooltip>
+          </span>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content
+            align="end"
+            className="bg-darkGrey rounded-lg shadow-lg z-50">
+            {!isOld && (
+              <DropdownMenu.Item onClick={() => setIsDeleteErrorVisible(true)}>
+                <Button icon={<Trash size={18} />} size="sm">
+                  Gliederungspunkt löschen
                 </Button>
               </DropdownMenu.Item>
-              <DropdownMenu.Item onClick={() => setAllNotLitigious()}>
-                <Button icon={<CheckCircle size={18} />} size="sm">
-                  Alle Beiträge als unstreitig markieren
-                </Button>
-              </DropdownMenu.Item>
-              <DropdownMenu.Item onClick={() => setAllLitigious()}>
-                <Button icon={<XCircle size={18} />} size="sm">
-                  Alle Beiträge als streitig markieren
-                </Button>
-              </DropdownMenu.Item>
-            </>
-          )}
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
+            )}
+            {user?.role === UserRole.Judge && (
+              <>
+                <DropdownMenu.Item onClick={() => resetLitigiousChecks()}>
+                  <Button icon={<Circle size={18} />} size="sm">
+                    Alle Streitigkeitsmarkierungen zurücksetzen
+                  </Button>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item onClick={() => setAllNotLitigious()}>
+                  <Button icon={<CheckCircle size={18} />} size="sm">
+                    Alle Beiträge als unstreitig markieren
+                  </Button>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item onClick={() => setAllLitigious()}>
+                  <Button icon={<XCircle size={18} />} size="sm">
+                    Alle Beiträge als streitig markieren
+                  </Button>
+                </DropdownMenu.Item>
+              </>
+            )}
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
+      <ErrorPopup isVisible={isDeleteErrorVisible}>
+        <div className="flex flex-col items-center justify-center space-y-8">
+          <p className="text-center text-base">
+            Sind Sie sicher, dass Sie den gesamten Gliederungspunkt mit allen
+            dazu aufgeführten Beiträgen löschen möchten? Diese Aktion kann nicht
+            rückgängig gemacht werden.
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Button
+              bgColor="bg-lightGrey hover:bg-mediumGrey/50"
+              textColor="text-mediumGrey font-bold hover:text-lightGrey"
+              onClick={() => {
+                setIsDeleteErrorVisible(false);
+              }}>
+              Abbrechen
+            </Button>
+            <Button
+              bgColor="bg-lightRed hover:bg-darkRed/25"
+              textColor="text-darkRed font-bold"
+              onClick={() => {
+                setIsDeleteErrorVisible(false);
+                deleteSection();
+              }}>
+              Gliederungspunkt löschen
+            </Button>
+          </div>
+        </div>
+      </ErrorPopup>
+    </>
   );
 };
