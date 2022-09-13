030b0e77b8bebb3fe1124ed903cb686e7bbbb5d2
markusbink <bink-markus@web.de>
2022-09-13 12:11:38 +0200

    #115 Remove new entry button in sorting

--- src/components/JudgeDiscussion.tsx
+++ src/components/JudgeDiscussion.tsx
@@ -64,7 +64,6 @@ export const JudgeDiscussion = () => {
                   );
                 })}
               </div>
-              <AddEntryButtons sectionId={section.id} />
             </div>
           );
         }
