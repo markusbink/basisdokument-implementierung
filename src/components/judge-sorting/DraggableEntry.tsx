aac119b34c8e26f3289e0deb2b5a2d0667268a7a
markusbink <bink-markus@web.de>
2022-09-16 09:09:47 +0200

    Add props to draggable entry

--- src/components/judge-sorting/DraggableEntry.tsx
+++ src/components/judge-sorting/DraggableEntry.tsx
@@ -1,6 +1,6 @@
 import cx from "classnames";
 import { useDrag } from "react-dnd";
-import { useCase, useUser } from "../../contexts";
+import { useBookmarks, useCase, useUser } from "../../contexts";
 import { getEntryById } from "../../contexts/CaseContext";
 import { IDragItemType } from "../../types";
 import { Entry } from "../entry";
@@ -15,7 +15,9 @@ export const DraggableEntry = ({
   index: number;
 }) => {
   const { user } = useUser();
-  const { entries } = useCase();
+  const { entries, currentVersion } = useCase();
+  const { bookmarks } = useBookmarks();
+
   const entry = getEntryById(entries, entryId);
 
   const [{ isDragging }, drag] = useDrag(
@@ -36,7 +38,22 @@ export const DraggableEntry = ({
         "outline-dotted outline-offset-4 rounded cursor-grabbing": isDragging,
         "cursor-grab": !isDragging,
       })}>
-      <>{entry && <Entry viewedBy={user!.role} entry={entry} />}</>
+      <>
+        {entry && (
+          <Entry
+            viewedBy={user!.role}
+            entry={entry}
+            isOld={entry.version < currentVersion}
+            isBookmarked={
+              bookmarks.find(
+                (bookmark) => bookmark.associatedEntry === entry.id
+              )
+                ? true
+                : false
+            }
+          />
+        )}
+      </>
     </div>
   );
 };
