fb132403b37687deff973c3e04e4685d467a4cae
markusbink <bink-markus@web.de>
2022-09-20 11:20:55 +0200

    #143 Move element on drop instead of on hover
    Resolves bug with the drag layer snapping back to initial position when releasing the drag

--- src/components/judge-sorting/DraggableEntry.tsx
+++ src/components/judge-sorting/DraggableEntry.tsx
@@ -74,10 +74,10 @@ export const DraggableEntry: React.FC<DraggableEntryProps> = ({
     }),
   });
 
-  const [{ handlerId, isOver, canDrop }, drop] = useDrop<
+  const [{ handlerId, isOver }, drop] = useDrop<
     DragItem,
     void,
-    { handlerId: any; isOver: boolean; canDrop: boolean }
+    { handlerId: any; isOver: boolean }
   >({
     accept: IDragItemType.ENTRY,
     canDrop(_: any, monitor) {
@@ -89,11 +89,10 @@ export const DraggableEntry: React.FC<DraggableEntryProps> = ({
     collect(monitor) {
       return {
         handlerId: monitor.getHandlerId(),
-        isOver: !!monitor.isOver(),
-        canDrop: !!monitor.canDrop(),
+        isOver: !!monitor.isOver() && monitor.getItem().index !== index,
       };
     },
-    hover(item, monitor) {
+    drop(item: DragItem, monitor) {
       if (!ref.current) {
         return;
       }
@@ -101,45 +100,8 @@ export const DraggableEntry: React.FC<DraggableEntryProps> = ({
       const dragIndex = item.index;
       const hoverIndex = index;
 
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-
-      // Determine rectangle on screen
-      const hoverBoundingRect = ref.current?.getBoundingClientRect();
-
-      // Get vertical middle
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-      // Determine mouse position
-      const clientOffset = monitor.getClientOffset();
-
-      // Get pixels to the top
-      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
-
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-
-      // Time to actually perform the action
       moveEntry(dragIndex, hoverIndex);
 
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
       item.index = hoverIndex;
     },
   });
@@ -152,6 +114,7 @@ export const DraggableEntry: React.FC<DraggableEntryProps> = ({
       className={cx({
         "cursor-grab": !isDragging,
         "cursor-grabbing": isDragging,
+        "opacity-50 offset-2 outline-dashed rounded-lg": isOver,
       })}
       data-handler-id={handlerId}>
       <>
