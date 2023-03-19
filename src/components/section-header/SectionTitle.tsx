5ea8e919c9315c0d3fd24c4e629ac01d5942ed1f
Martina Emmert <tina.emmert@web.de>
2023-03-19 16:25:51 +0100

    #10 add button for suggestions

--- src/components/section-header/SectionTitle.tsx
+++ src/components/section-header/SectionTitle.tsx
@@ -1,4 +1,6 @@
+import { Tooltip } from "../Tooltip";
 import cx from "classnames";
+import { Signpost } from "phosphor-react";
 import { useEffect, useRef, useState } from "react";
 import { useCase, useHeaderContext, useSection, useUser } from "../../contexts";
 import { getTheme } from "../../themes/getTheme";
@@ -18,6 +20,7 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
   version,
 }) => {
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [onFocus, setOnFocus] = useState<boolean>(false);
   const { user } = useUser();
   const { setSectionList } = useSection();
   const { currentVersion } = useCase();
@@ -54,7 +57,7 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
   }, [title]);
 
   return (
-    <div className={cx("flex w-full flex-col pt-2")}>
+    <div className={cx("flex w-full flex-col")}>
       <span
         className={cx(
           "text-xs font-bold rounded-md px-2 py-1 w-fit uppercase text-darkGrey",
@@ -71,56 +74,80 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
       </span>
       <div className={cx("flex items-start justify-between gap-2 w-full pt-3")}>
         {isEditing ? (
-          <input
-            ref={titleInputRef}
-            readOnly={
-              isOld || (role !== user?.role && user?.role !== UserRole.Judge)
-            }
-            placeholder={"Bisher kein Titel vergeben " + version}
-            type="text"
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                e.preventDefault();
-                if (title.length > 0) {
+          <div
+            className={cx(
+              "flex flex-row w-full rounded pointer-none outline-2 outline-offset-2",
+              {
+                "outline outline-lightGrey":
+                  onFocus &&
+                  (role === user?.role || user?.role === UserRole.Judge) &&
+                  !isOld,
+                "hover:outline hover:outline-lightGrey":
+                  (role === user?.role || user?.role === UserRole.Judge) &&
+                  !isOld,
+                "outline-none": !onFocus,
+              }
+            )}>
+            <input
+              autoFocus
+              ref={titleInputRef}
+              onFocus={() => {
+                setOnFocus(true);
+              }}
+              readOnly={
+                isOld || (role !== user?.role && user?.role !== UserRole.Judge)
+              }
+              placeholder={"Bisher kein Titel vergeben"}
+              type="text"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  if (title.trim().length > 0) {
+                    setIsEditing(false);
+                  }
+                  e.currentTarget.blur();
+                }
+              }}
+              onChange={changeTitle}
+              onBlur={() => {
+                if (title.trim().length > 0) {
                   setIsEditing(false);
                 }
-                e.currentTarget.blur();
-              }
-            }}
-            onChange={changeTitle}
-            onBlur={() => {
-              if (title.length > 0) {
-                setIsEditing(false);
-              }
-            }}
-            value={title}
-            className={cx(
-              "bg-transparent text-xl font-bold w-full outline-none rounded",
-              {
-                "focus:outline focus:outline-offset-2 focus:outline-darkGrey":
-                  (role === user?.role || user?.role === UserRole.Judge) &&
-                  !isOld,
-                "hover:cursor-not-allowed":
-                  (role !== user?.role && user?.role !== UserRole.Judge) ||
-                  isOld,
-              }
-            )}
-          />
+                setOnFocus(false);
+              }}
+              value={title}
+              className={cx(
+                "bg-transparent text-xl font-bold w-full outline-none rounded",
+                {
+                  "hover:cursor-not-allowed":
+                    (role !== user?.role && user?.role !== UserRole.Judge) ||
+                    isOld,
+                }
+              )}></input>
+            {onFocus &&
+              (role === user?.role || user?.role === UserRole.Judge) &&
+              !isOld && (
+                <Tooltip text="Vorschläge" className="justify-end mx-2">
+                  <Signpost
+                    className="text-darkGrey hover:text-mediumGrey"
+                    size={16}></Signpost>
+                </Tooltip>
+              )}
+          </div>
         ) : (
           <h2
-            className={cx(
-              "bg-transparent text-xl font-bold outline-offset-[6px] rounded",
-              {
-                "hover:outline hover:outline-offset-2 hover:outline-blue-600":
-                  (role === user?.role || user?.role === UserRole.Judge) &&
-                  !isOld,
-                "hover:cursor-not-allowed":
-                  (role !== user?.role && user?.role !== UserRole.Judge) ||
-                  isOld,
-              }
-            )}
+            className={cx("bg-transparent text-xl font-bold rounded w-full", {
+              "hover:outline hover:outline-lightGrey hover:outline-2 hover:outline-offset-2":
+                (role === user?.role || user?.role === UserRole.Judge) &&
+                !isOld,
+              "hover:cursor-not-allowed":
+                (role !== user?.role && user?.role !== UserRole.Judge) || isOld,
+            })}
             onClick={() => {
-              if (!isOld) {
+              if (
+                (role === user?.role || user?.role === UserRole.Judge) &&
+                !isOld
+              ) {
                 setIsEditing(true);
               }
             }}>
