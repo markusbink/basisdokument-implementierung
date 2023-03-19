452358bd6cbcb64f55b085e54d2c980223e1b937
Martina Emmert <tina.emmert@web.de>
2023-03-19 21:16:03 +0100

    #10 add suggestions

--- src/components/section-header/SectionTitle.tsx
+++ src/components/section-header/SectionTitle.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 import { useCase, useHeaderContext, useSection, useUser } from "../../contexts";
 import { getTheme } from "../../themes/getTheme";
 import { UserRole } from "../../types";
+import { useOutsideClick } from "../../hooks/use-outside-click";
 
 interface SectionTitleProps {
   id: string;
@@ -20,7 +21,8 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
   version,
 }) => {
   const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [onFocus, setOnFocus] = useState<boolean>(false);
+  const [suggestionsActive, setSuggestionsActive] = useState<boolean>(false);
+
   const { user } = useUser();
   const { setSectionList } = useSection();
   const { currentVersion } = useCase();
@@ -30,17 +32,17 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
     !(typeof title === "string" && title.trim().length === 0);
   const titleInputRef = useRef<HTMLInputElement>(null);
 
-  const changeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const changeTitle = (t: string) => {
     setSectionList((prevState) => {
       const newState = [...prevState];
       const section = newState.find((s) => s.id === id);
 
       if (section) {
         if (role === UserRole.Plaintiff) {
-          section.titlePlaintiff = e.target.value;
+          section.titlePlaintiff = t;
           section.titlePlaintiffVersion = currentVersion;
         } else {
-          section.titleDefendant = e.target.value;
+          section.titleDefendant = t;
           section.titleDefendantVersion = currentVersion;
         }
       }
@@ -55,6 +57,15 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
       setIsEditing(true);
     }
   }, [title]);
+
+  const menuRef = useRef(null);
+  useOutsideClick(menuRef, (e) => {
+    if (title.trim().length > 0) {
+      setIsEditing(false);
+    }
+    setSuggestionsActive(false);
+    e?.stopPropagation();
+  });
 
   return (
     <div className={cx("flex w-full flex-col")}>
@@ -72,28 +83,26 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
         )}>
         {role}
       </span>
-      <div className={cx("flex items-start justify-between gap-2 w-full pt-3")}>
+      <div
+        ref={menuRef}
+        className={cx("flex items-start justify-between gap-2 w-full pt-3")}>
         {isEditing ? (
           <div
             className={cx(
-              "flex flex-row w-full rounded pointer-none outline-2 outline-offset-2",
+              "flex flex-row justify-between w-full rounded pointer-none outline-none outline-2 outline-offset-2",
               {
                 "outline outline-lightGrey":
-                  onFocus &&
+                  suggestionsActive &&
                   (role === user?.role || user?.role === UserRole.Judge) &&
                   !isOld,
                 "hover:outline hover:outline-lightGrey":
                   (role === user?.role || user?.role === UserRole.Judge) &&
                   !isOld,
-                "outline-none": !onFocus,
               }
             )}>
             <input
               autoFocus
               ref={titleInputRef}
-              onFocus={() => {
-                setOnFocus(true);
-              }}
               readOnly={
                 isOld || (role !== user?.role && user?.role !== UserRole.Judge)
               }
@@ -105,44 +114,58 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
                   if (title.trim().length > 0) {
                     setIsEditing(false);
                   }
-                  e.currentTarget.blur();
+                  setSuggestionsActive(false);
                 }
               }}
-              onChange={changeTitle}
+              onChange={(e) => changeTitle(e.target.value)}
               onBlur={() => {
                 if (title.trim().length > 0) {
                   setIsEditing(false);
                 }
-                setOnFocus(false);
+                setSuggestionsActive(false);
               }}
               value={title}
               className={cx(
-                "bg-transparent text-xl font-bold w-full outline-none rounded",
+                "bg-transparent text-xl font-bold w-[100px] outline-none rounded",
                 {
                   "hover:cursor-not-allowed":
                     (role !== user?.role && user?.role !== UserRole.Judge) ||
                     isOld,
                 }
               )}></input>
-            {onFocus &&
-              (role === user?.role || user?.role === UserRole.Judge) &&
+            {(role === user?.role || user?.role === UserRole.Judge) &&
               !isOld && (
                 <Tooltip text="Vorschläge" className="justify-end mx-2">
-                  <Signpost
-                    className="text-darkGrey hover:text-mediumGrey"
-                    size={16}></Signpost>
+                  <div
+                    className={cx("rounded p-1 text-mediumGrey opacity-50", {
+                      "bg-lightGrey": suggestionsActive,
+                    })}
+                    onClick={(e) => {
+                      setSuggestionsActive(!suggestionsActive);
+                      e.stopPropagation();
+                    }}>
+                    <Signpost size={16}></Signpost>
+                  </div>
                 </Tooltip>
               )}
           </div>
         ) : (
           <h2
-            className={cx("bg-transparent text-xl font-bold rounded w-full", {
-              "hover:outline hover:outline-lightGrey hover:outline-2 hover:outline-offset-2":
-                (role === user?.role || user?.role === UserRole.Judge) &&
-                !isOld,
-              "hover:cursor-not-allowed":
-                (role !== user?.role && user?.role !== UserRole.Judge) || isOld,
-            })}
+            className={cx(
+              "flex flex-row justify-between bg-transparent text-xl font-bold rounded w-full outline-2 outline-offset-2",
+              {
+                "outline outline-lightGrey":
+                  suggestionsActive &&
+                  (role === user?.role || user?.role === UserRole.Judge) &&
+                  !isOld,
+                "hover:outline hover:outline-lightGrey":
+                  (role === user?.role || user?.role === UserRole.Judge) &&
+                  !isOld,
+                "hover:cursor-not-allowed":
+                  (role !== user?.role && user?.role !== UserRole.Judge) ||
+                  isOld,
+              }
+            )}
             onClick={() => {
               if (
                 (role === user?.role || user?.role === UserRole.Judge) &&
@@ -152,8 +175,78 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
               }
             }}>
             {title}
+            {(role === user?.role || user?.role === UserRole.Judge) &&
+              !isOld && (
+                <Tooltip text="Vorschläge" className="justify-end mx-2">
+                  <div
+                    className={cx("rounded p-1 text-mediumGrey opacity-50", {
+                      "bg-lightGrey": suggestionsActive,
+                    })}
+                    onClick={(e) => {
+                      setSuggestionsActive(!suggestionsActive);
+                      e.stopPropagation();
+                    }}>
+                    <Signpost size={16}></Signpost>
+                  </div>
+                </Tooltip>
+              )}
           </h2>
         )}
+        <div className="relative">
+          {suggestionsActive ? (
+            <ul
+              className="absolute right-0 top-2 p-2 bg-offWhite text-darkGrey rounded-xl w-max h-[180px] overflow-auto shadow-lg z-50 font-medium"
+              onClick={(e) => {
+                let value = (e.target as HTMLElement).innerHTML;
+                if (value && value.length > 0) {
+                  changeTitle(value);
+                  setIsEditing(false);
+                }
+                setSuggestionsActive(false);
+              }}>
+              <li
+                tabIndex={0}
+                className="p-1 rounded-lg hover:bg-lightGrey focus:bg-lightGrey focus:outline-none cursor-pointer">
+                Anträge
+              </li>
+              <li
+                tabIndex={0}
+                className="p-1 rounded-lg hover:bg-lightGrey focus:bg-lightGrey focus:outline-none cursor-pointer">
+                Sachvortrag
+              </li>
+              <li
+                tabIndex={0}
+                className="p-1 rounded-lg hover:bg-lightGrey focus:bg-lightGrey focus:outline-none cursor-pointer">
+                Rechtliche Würdigung
+              </li>
+              <li
+                tabIndex={0}
+                className="p-1 rounded-lg hover:bg-lightGrey focus:bg-lightGrey focus:outline-none cursor-pointer">
+                Zulässigkeit
+              </li>
+              <li
+                tabIndex={0}
+                className="p-1 rounded-lg hover:bg-lightGrey focus:bg-lightGrey focus:outline-none cursor-pointer">
+                Begründetheit
+              </li>
+              <li
+                tabIndex={0}
+                className="p-1 rounded-lg hover:bg-lightGrey focus:bg-lightGrey focus:outline-none cursor-pointer">
+                Zum Anspruchsgrund
+              </li>
+              <li
+                tabIndex={0}
+                className="p-1 rounded-lg hover:bg-lightGrey focus:bg-lightGrey focus:outline-none cursor-pointer">
+                Zur Anspruchshöhe
+              </li>
+              <li
+                tabIndex={0}
+                className="p-1 rounded-lg hover:bg-lightGrey focus:bg-lightGrey focus:outline-none cursor-pointer">
+                Vorbemerkung
+              </li>
+            </ul>
+          ) : null}
+        </div>
       </div>
     </div>
   );
