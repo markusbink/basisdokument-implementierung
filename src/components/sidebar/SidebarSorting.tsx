c49d8b3b7142bc593f81055b030c86c92f406298
Julia Sageder <julia.sageder@informatik.uni-regensburg.de>
2023-04-20 10:40:47 +0200

    #20 fix overflow bug in sorting sidebar

--- src/components/sidebar/SidebarSorting.tsx
+++ src/components/sidebar/SidebarSorting.tsx
@@ -42,7 +42,8 @@ export const SidebarSorting = () => {
   };
 
   return (
-    <div className="flex flex-col gap-3 flex-1 overflow-auto">
+    //h-[calc(100vh-56px)] -> overflow scroll needs a fixed height of parent: 56px (height of sidebar header)
+    <div className="flex flex-col gap-3 h-[calc(100vh-56px)]">
       <div className="flex flex-row justify-between pt-4 px-4">
         <div className="font-bold text-darkGrey text-lg">Gliederung:</div>
         <div className="content-center">
@@ -69,7 +70,7 @@ export const SidebarSorting = () => {
           Es wurden noch keine Gliederungspunkte angelegt.
         </span>
       )}
-      <div className="px-4">
+      <div className="px-4 mb-4 flex-1 overflow-y-scroll scroll-smooth">
         {selectedSorting === Sorting.Privat ? (
           // private sorting
           <>
